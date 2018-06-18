577aef37aad12762f215d21b78741e9c701fdf35
Roby Parr <roby.parr@gmail.com>
2018-06-18 22:51:34 +0900

    Fix production configuration.
    - Production configuration wasn't setup correctly for SSL which
    is causing some slower response times from the client. Even though
    the logs were showing 10s of milliseconds or even microseconds,
    response times were averaging between 300-600 ms, sometimes over
    1 second, roundtrip from the client. That average seems just a
    little too long to be completely due to latency.
    
    - Remove prod.secret.exs as we're using envrionment variables
    anyways.

--- config/prod.exs
+++ config/prod.exs
@@ -15,9 +15,17 @@ use Mix.Config
 # which you typically run after static files are built.
 config :barcode_service, BarcodeServiceWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: System.get_env("HOST"), port: 80],
+  url: [scheme: "https", host: System.get_env("HOST"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+config :barcode_service, BarcodeService.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 15,
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -30,45 +38,3 @@ config :scout_apm,
 config :phoenix, :template_engines,
   eex: ScoutApm.Instruments.EExEngine,
   exs: ScoutApm.Instruments.ExsEngine
-
-# ## SSL Support
-#
-# To get SSL working, you will need to add the `https` key
-# to the previous section and set your `:url` port to 443:
-#
-#     config :barcode_service, BarcodeServiceWeb.Endpoint,
-#       ...
-#       url: [host: "example.com", port: 443],
-#       https: [:inet6,
-#               port: 443,
-#               keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#               certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
-#
-# Where those two env variables return an absolute path to
-# the key and cert in disk or a relative path inside priv,
-# for example "priv/ssl/server.key".
-#
-# We also recommend setting `force_ssl`, ensuring no data is
-# ever sent via http, always redirecting to https:
-#
-#     config :barcode_service, BarcodeServiceWeb.Endpoint,
-#       force_ssl: [hsts: true]
-#
-# Check `Plug.SSL` for all available options in `force_ssl`.
-
-# ## Using releases
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start the server for all endpoints:
-#
-#     config :phoenix, :serve_endpoints, true
-#
-# Alternatively, you can configure exactly which server to
-# start per endpoint:
-#
-#     config :barcode_service, BarcodeServiceWeb.Endpoint, server: true
-#
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
